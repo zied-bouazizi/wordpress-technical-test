3ebbb0a9827e4d83aef68078ea6e53e5f544017a
Zied Bouazizi <zied.bouazizi@outlook.com>
2023-01-29 18:05:59 +0100

    Added newtheme

---
+++ wp-content/themes/newtheme/index.php
@@ -0,0 +1,41 @@
+<?php get_header(); ?>
+
+<section class="container-fluid custom_box">
+  <div class="container">
+
+    <div class="banner">
+      <div class="banner_overlay">
+
+        <div class="custom_menu">
+          <div class="logo">
+            <h4 class="logo">WordPress Technical Test</h4>
+          </div>
+          <div class="barr">
+            <i class="fa fa-bars"></i>
+          </div>
+        </div>
+
+        <header>
+          <div class="header_text">
+            <div class="first_subtitle">
+              <h4>Job Listing</h4>
+            </div>
+            <div class="main_title">
+              <h1>WordPress Technical Test</h1>
+            </div>
+            <div class="read_more">
+              <a class="btn btn-default btn_csutom" href="#">Read More</a>
+            </div>
+          </div>
+        </header>
+
+      </div>
+    </div>
+
+  </div>
+</section>
+
+<div class="container container_filter">
+</div>
+
+<?php get_footer(); ?>
