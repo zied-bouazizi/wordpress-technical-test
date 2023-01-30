b52c91af96adcbd909a56c2ccbf051a4cc643717
Zied Bouazizi <zied.bouazizi@outlook.com>
2023-01-30 09:54:13 +0100

    Added custom fields for job

--- wp-content/themes/newtheme/functions.php
+++ wp-content/themes/newtheme/functions.php
@@ -84,3 +84,133 @@ function create_job_function()
 }
 
 add_action('init', 'create_job_function');
+
+// Meta Box Class: CustomFieldsMetaBox
+class CustomFieldsMetaBox
+{
+
+    private $screen = array(
+        'job',
+
+    );
+
+    private $meta_fields = array(
+        array(
+            'label' => 'Address',
+            'id' => 'address',
+            'type' => 'text',
+        ),
+
+        array(
+            'label' => 'Working Hour Per Week',
+            'id' => 'working_hour_per_week',
+            'type' => 'number',
+        ),
+
+        array(
+            'label' => 'Recruiting Company',
+            'id' => 'recruiting_company',
+            'type' => 'text',
+        ),
+
+        array(
+            'label' => 'Company Email',
+            'id' => 'company_email',
+            'type' => 'email',
+        ),
+
+        array(
+            'label' => 'Offer Expiration Date',
+            'id' => 'offer_expiration_date',
+            'type' => 'date',
+        )
+
+    );
+
+    public function __construct()
+    {
+        add_action('add_meta_boxes', array($this, 'add_meta_boxes'));
+        add_action('save_post', array($this, 'save_fields'));
+    }
+
+    public function add_meta_boxes()
+    {
+        foreach ($this->screen as $single_screen) {
+            add_meta_box(
+                'Custom Fields',
+                __('Custom Fields', ''),
+                array($this, 'meta_box_callback'),
+                $single_screen,
+                'normal',
+                'default'
+            );
+        }
+    }
+
+    public function meta_box_callback($post)
+    {
+        wp_nonce_field('CustomFields_data', 'CustomFields_nonce');
+        $this->field_generator($post);
+    }
+    public function field_generator($post)
+    {
+        $output = '';
+        foreach ($this->meta_fields as $meta_field) {
+            $label = '<label for="' . $meta_field['id'] . '">' . $meta_field['label'] . '</label>';
+            $meta_value = get_post_meta($post->ID, $meta_field['id'], true);
+            if (empty($meta_value)) {
+                if (isset($meta_field['default'])) {
+                    $meta_value = $meta_field['default'];
+                }
+            }
+            switch ($meta_field['type']) {
+                default:
+                    $input = sprintf(
+                        '<input %s id="%s" name="%s" type="%s" value="%s">',
+                        $meta_field['type'] !== 'color' ? 'style="width: 100%"' : '',
+                        $meta_field['id'],
+                        $meta_field['id'],
+                        $meta_field['type'],
+                        $meta_value
+                    );
+            }
+            $output .= $this->format_rows($label, $input);
+        }
+        echo '<table class="form-table"><tbody>' . $output . '</tbody></table>';
+    }
+
+    public function format_rows($label, $input)
+    {
+        return '<tr><th>' . $label . '</th><td>' . $input . '</td></tr>';
+    }
+
+    public function save_fields($post_id)
+    {
+        if (!isset($_POST['CustomFields_nonce']))
+            return $post_id;
+        $nonce = $_POST['CustomFields_nonce'];
+        if (!wp_verify_nonce($nonce, 'CustomFields_data'))
+            return $post_id;
+        if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE)
+            return $post_id;
+        foreach ($this->meta_fields as $meta_field) {
+            if (isset($_POST[$meta_field['id']])) {
+                switch ($meta_field['type']) {
+                    case 'email':
+                        $_POST[$meta_field['id']] = sanitize_email($_POST[$meta_field['id']]);
+                        break;
+                    case 'text':
+                        $_POST[$meta_field['id']] = sanitize_text_field($_POST[$meta_field['id']]);
+                        break;
+                }
+                update_post_meta($post_id, $meta_field['id'], $_POST[$meta_field['id']]);
+            } else if ($meta_field['type'] === 'checkbox') {
+                update_post_meta($post_id, $meta_field['id'], '0');
+            }
+        }
+    }
+}
+
+if (class_exists('CustomFieldsMetabox')) {
+    new CustomFieldsMetabox;
+};
